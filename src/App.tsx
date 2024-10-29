be53e4ce748f6558becc1318ae3c636c5e5180ff
Jack Rong <jackrong03@gmail.com>
2024-10-29 21:46:56 +0000

    feat(PIGEON-2): initial guest and host button and install formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import './App.css'
+import Guest from './components/Guest';
 
 function App() {
+  const [isHost, setIsHost] = useState<boolean | undefined>(undefined);
 
   return (
-    <h1 className='p-4 bg-blue-500 text-white font-bold'>Test</h1>
+    <>
+        <header className='font-bold text-5xl text-center p-4'>
+          PIGEON
+        </header>
+
+        <main className='min-h-screen flex flex-col justify-center items-center'>
+
+        {isHost === undefined && (
+          <div className='flex'>
+            <button onClick={() => setIsHost(false)} className='bg-black text-white m-4 p-4 rounded-lg text-3xl hover:underline'>Guest</button>
+            <button onClick={() => setIsHost(true)} className='bg-black text-white m-4 p-4 rounded-lg text-3xl hover:underline'>Host</button>
+          </div>
+          )}
+
+        {isHost && (
+          <div>
+            <p className='text-3xl'>Host</p>
+          </div>
+        )}
+
+        {isHost === false && Guest }
+        </main>
+    </>    
   )
 }
 
